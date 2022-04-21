414a7a526dea89cc99b78c652f6ab895f70f6028
Valentin Zonov <v.zonov@pressfoto.ru>
2022-04-21 12:21:49 +0700

    add sudo to clear_all file

--- clear_all[NOT FOR PRODUCTION].sh
+++ clear_all[NOT FOR PRODUCTION].sh
@@ -3,7 +3,11 @@
 while true; do
     read -p "Do you wish to remove ALL docker containers and images?(y/n) " yn
     case $yn in
-        [Yy]* ) sudo docker stop $(docker ps -a -q); sudo docker rm $(docker ps -a -q) --force; sudo docker rmi $(docker images -a -q) --force; sudo docker system prune -y; break;;
+        [Yy]* ) 
+			sudo docker stop $(sudo docker ps -a -q); 
+			sudo docker rm $(sudo docker ps -a -q) --force; 
+			sudo docker rmi $(sudo docker images -a -q) --force; 
+			sudo docker system prune --force; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
