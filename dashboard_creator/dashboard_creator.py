fbd46b99a24d1e3d1c93b9b5c735319b7946d560
BarMa <zov_13@mail.ru>
2022-06-20 01:56:13 +0700

    added dashboard creation in grafana

--- dashboard_creator/dashboard_creator.py
+++ dashboard_creator/dashboard_creator.py
@@ -7,9 +7,11 @@ import argparse
 import pymysql
 import time
 import configparser
+import copy
 
-headers = {'Accept': 'application/json', 'Content-Type': 'application/json; charset=UTF-8', 'Authorization': 'Bearer eyJrIjoidDBzNzZOOHl1cXpNaU5vemZteWJ6a0lRdkMxUnZzVXEiLCJuIjoic3RhdGlzdGljc19nYXRoZXJfa2V5IiwiaWQiOjF9'}
-
+headers = {'Accept': 'application/json', 'Content-Type': 'application/json; charset=UTF-8'}
+grafana_conatiner_name = 'grafana_container'
+global connection
 
 def get_new_dashboards()->dict:
     new_dashboards = {}
@@ -17,11 +19,11 @@ def get_new_dashboards()->dict:
         sql = "SELECT id, source_name FROM data_source"
         cursor.execute(sql)
         sources = cursor.fetchall()
-        print(sources)
+        print("Sources: ", sources)
         for source in sources:
             print(source)
-            print(f"https://metal-glasses-peel-90-188-10-160.loca.lt/api/dashboards/{source['source_name']}")
-            response = requests.get(f"https://metal-glasses-peel-90-188-10-160.loca.lt/api/search?query={source['source_name']}", headers=headers)
+            print(f"http://admin:adminpw@{grafana_conatiner_name}:3000/api/dashboards/{source['source_name']}")
+            response = requests.get(f"http://admin:adminpw@{grafana_conatiner_name}:3000/api/search?query={source['source_name']}", headers=headers)
             print(response.json())
             if not response.json():
                 new_dashboards[source['id']] = source['source_name']
@@ -36,7 +38,13 @@ def get_panels(source_id)->dict:
         fields = cursor.fetchall()
         for field in fields:
             raw_sql[field['field_name']] = "SELECT UNIX_TIMESTAMP(CONVERT_TZ(date,'+00:00','-7:00')) as time, field_value as value FROM data_main WHERE (UNIX_TIMESTAMP(CONVERT_TZ(date,'+00:00','-7:00')) BETWEEN ${__from:date:seconds} AND ${__to:date:seconds}) AND data_main.field_id = %d ORDER BY date ASC;" % int(field['id'])
+            print(field['field_name'],":",raw_sql[field['field_name']])
     return raw_sql
+
+
+def get_next_panel_id()->int:
+    response = requests.get(f"http://admin:adminpw@{grafana_conatiner_name}:3000/api/search?query={source['source_name']}", headers=headers)
+    pass
 
 
 def add_new_dashboards(dashboards, template)->None:
@@ -45,17 +53,25 @@ def add_new_dashboards(dashboards, template)->None:
         if len(template["dashboard"]["panels"]) == 0:
             logging.error('template has no panels')
             exit(-1)
-        panel = template["dashboard"]["panels"][0]
-        if len(panel["targets"][0]["rawSql"]) == 0:
+        panel_template = template["dashboard"]["panels"][0]
+        if len(panel_template["targets"][0]["rawSql"]) == 0:
             logging.error('template has no raw sql')
             exit(-1)
-        template["panels"] = []
+        template["dashboard"]["panels"] = []
+        panel_id = 1
         for title in fields:
-            panel["title"] = title
-            panel["targets"][0]["rawSql"] = fields[title]
-            template["dashboard"]["panels"].append(panel)
+            print(type(template["dashboard"]["panels"]))
+            print(title)
+            panel_temp = copy.deepcopy(panel_template)
+            panel_temp["title"] = title
+            panel_temp["targets"][0]["rawSql"] = fields[title]
+            panel_temp["gridPos"]["y"] = (panel_id - 1) * 8
+            panel_temp["id"] = panel_id
+            panel_id += 1
+            template["dashboard"]["panels"].append(panel_temp)
         template["dashboard"]["title"] = dashboards[source_id]
-        response = requests.post('https://metal-glasses-peel-90-188-10-160.loca.lt/api/dashboards/db', headers=headers, json=template)
+        print(template["dashboard"]["panels"])
+        response = requests.post(f'http://admin:adminpw@{grafana_conatiner_name}:3000/api/dashboards/db', headers=headers, json=template)
         print(response.status_code, response.content)
 
 
@@ -85,22 +101,24 @@ if __name__ == "__main__":
                                      database=config['MySQL']['database'],
                                      port=int(config['MySQL']['port']),
                                      charset='utf8mb4',
-                                     cursorclass=pymysql.cursors.DictCursor)
+                                     cursorclass=pymysql.cursors.DictCursor,
+                                     autocommit=True)
 
         dashboard_template = {}
         with open(template_path, 'r') as dashboard_template_file:
             template = json.load(dashboard_template_file)
+            print(type(template))
             print(template)
 
         interval = int(config["OTHER"]["interval"])
         if not interval:
             interval = 300 # 2 minutes default
         while True:
-            print('start')
+            print(time.strftime("%H:%M:%S", time.localtime()), 'start')
             dashboards = get_new_dashboards()
             if dashboards:
                 add_new_dashboards(dashboards,template)
-            print('end')
+            print(time.strftime("%H:%M:%S", time.localtime()), 'end')
             time.sleep(interval)
     except Exception as e:
         print(e)
